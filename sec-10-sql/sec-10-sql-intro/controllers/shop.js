0ad4703c010467340d9550a2f42b2547fc446a02
Jack Gamboa <jgamboa929@gmail.com>
2019-06-24 10:13:48 -0500

    product.js - get product details.

--- sec-10-sql/sec-10-sql-intro/controllers/shop.js
+++ sec-10-sql/sec-10-sql-intro/controllers/shop.js
@@ -17,13 +17,18 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products"
+  Product.findById(prodId)
+    .then(([product, fieldData]) => {
+      // console.log(product); // array is returned
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product.title,
+        path: "/products"
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
 };
 
 exports.getIndex = (req, res, next) => {
