81621525dee530141898d0c3b533472e8d4765e6
Jack Gamboa <jgamboa929@gmail.com>
2019-07-12 17:23:35 -0500

    sec03 - app.js - write to file.

--- sec03-node-basics/nodejs/app.js
+++ sec03-node-basics/nodejs/app.js
@@ -18,9 +18,33 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
+    // How do you work with chunks of data? Your code works with a buffer to organize these chunks.
+    // Stream -> Req Body Pt 1 -> Req Body Pt 2 -> Req Body Pt 3 -> Fully Parsed
+    // Buffer: (Req Body Pt 2 -> Req Body Pt 3)
+
+    const body = [];
+
+    // .on('data', {}) method binds an event to an object.  'Data' event fired whenever a new chunk is ready to be read.  We receive a 'chunk' of data.
+    req.on("data", chunk => {
+      console.log(chunk); // <Buffer 6d 65 73 73 61 67 65 3d 61 61 61>
+      body.push(chunk);
+    });
+
+    // Fired whenever the incoming request is done.  At this point we can now rely on all of the chunks being read in.
+    // To work with these chunks we now have to Buffer them to be able to work with them.
+    // Buffer is available globally by Nodejs
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString(); // Add all your chunks from inside body [chunk1, chunk2, chunk3, ...] to the Buffer and .toString().
+      console.log(parsedBody); // message=aaa; 'message' is the name of our text input we defined.
+
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync("message.txt", message);
+    });
+
+    // res.writeHead(302, { Location: "/" });
+    res.statusCode = 302; // 302 Found is a common way of performing URL redirection
+    res.setHeader("Location", "/"); // "Location" is a default header accepted by the browser
     return res.end();
   }
 
