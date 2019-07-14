737ba7171af1461cc855be39bc6e14ca2a7b626b
Jack Gamboa <jgamboa929@gmail.com>
2019-07-14 15:55:44 -0500

    sec03_app.js -  handle async code.

--- sec03-node-basics/nodejs/app.js
+++ sec03-node-basics/nodejs/app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // console.log(req.url, req.method, req.headers);
-  console.log(req.url);
+  // console.log(req.url);
 
   const url = req.url;
   const method = req.method;
@@ -26,26 +26,30 @@ const server = http.createServer((req, res) => {
 
     // .on('data', {}) method binds an event to an object.  'Data' event fired whenever a new chunk is ready to be read.  We receive a 'chunk' of data.
     req.on("data", chunk => {
-      console.log(chunk); // <Buffer 6d 65 73 73 61 67 65 3d 61 61 61>
+      console.log("chunk: ", chunk); // <Buffer 6d 65 73 73 61 67 65 3d 61 61 61>
       body.push(chunk);
     });
 
     // Fired whenever the incoming request is done.  At this point we can now rely on all of the chunks being read in.
     // To work with these chunks we now have to Buffer them to be able to work with them.
     // Buffer is available globally by Nodejs
-    req.on("end", () => {
+    // Add a 'return' below so it does not execute lines after this block of code.
+    return req.on("end", () => {
+      console.log("Buffer.concat(body): ", Buffer.concat(body));
       const parsedBody = Buffer.concat(body).toString(); // Add all your chunks from inside body [chunk1, chunk2, chunk3, ...] to the Buffer and .toString().
-      console.log(parsedBody); // message=aaa; 'message' is the name of our text input we defined.
+      console.log("Buffer.concat(body).toString(): " + parsedBody); // message=aaa; 'message' is the name of our text input we defined.
 
       const message = parsedBody.split("=")[1];
 
-      fs.writeFileSync("message.txt", message);
+      // fs.writeFileSync("message.txt", message); // Use writeFile instead to run async code.
+      fs.writeFile("message.txt", message, err => {
+        // This response should be set only when we are done with the file.
+        // res.writeHead(302, { Location: "/" });
+        res.statusCode = 302; // 302 Found is a common way of performing URL redirection
+        res.setHeader("Location", "/"); // "Location" is a default header accepted by the browser
+        return res.end();
+      });
     });
-
-    // res.writeHead(302, { Location: "/" });
-    res.statusCode = 302; // 302 Found is a common way of performing URL redirection
-    res.setHeader("Location", "/"); // "Location" is a default header accepted by the browser
-    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
