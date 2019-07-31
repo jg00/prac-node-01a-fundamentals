3d1b42c32dcc245d1904eea5903313123b6f7ace
Jack Gamboa <jgamboa929@gmail.com>
2019-07-31 09:44:12 -0500

    sec05 app.js - set up express

--- sec05-expressjs/nodejs/app.js
+++ sec05-expressjs/nodejs/app.js
@@ -1,9 +1,31 @@
-const http = require("http");
+// const http = require("http");
 
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
+  next();
+});
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("In the add product middleware!");
+  // res.setHeader("content-type", "text/plain");
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In hello from express middleware!");
+  // res.setHeader("content-type", "text/plain");
+  res.send("<h1>Hello from Express Two!</h1>");
+});
+
+// 204: No Content
+app.get("/favicon.ico", (req, res, next) => res.status(204));
+
+app.listen(3000);
+
+// Works as well
+// const server = http.createServer(app);
+// server.listen(3000);
