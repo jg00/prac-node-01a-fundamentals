6ca7bf66db6545a7b4b16285d94f292d92eb8734
Jack Gamboa <jgamboa929@gmail.com>
2019-08-01 10:36:11 -0500

    a2_navigation - set up simple project complete.

--- sec05-expressjs/node_a2_navigation/app.js
+++ sec05-expressjs/node_a2_navigation/app.js
@@ -1,8 +1,11 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 const userRoutes = require("./routes/user");
 const mainRoutes = require("./routes/main");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", userRoutes);
 app.use(mainRoutes);
@@ -10,7 +13,7 @@ app.use(mainRoutes);
 app.use("/favicon.ico", (req, res, next) => res.status(204).end());
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>File Not Found</h1>");
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000, () => console.log("Server started!"));
--- sec05-expressjs/node_a2_navigation/routes/main.js
+++ sec05-expressjs/node_a2_navigation/routes/main.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const rootDir = require("../util/path");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "main.html"));
+  res.sendFile(path.join(rootDir, "views", "home.html"));
 });
 
 module.exports = router;
