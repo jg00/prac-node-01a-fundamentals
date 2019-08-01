e825a3c89be51823bd9f1b21f45c410051378a1f
Jack Gamboa <jgamboa929@gmail.com>
2019-07-31 21:29:38 -0500

    views: shop.html, add-product.html and css styles created.

---
+++ nodejs/routes/admin.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  // console.log("In the add product middleware!");
+  // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="author"><button type="submit">Add Product</button></form>');
+
+  // console.log("/add-product rootDir:", rootDir);
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res, next) => {
+  //   console.log("In the post /add-product");
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
