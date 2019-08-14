296ba20fd05835c2452de9f8103468e4ceeb73c9
Jack Gamboa <jgamboa929@gmail.com>
2019-08-14 12:10:27 -0500

    shop.ejs, add-product.ejs, 404.ejs - implement ejs.

--- sec06-dynamic-content/nodejs/app.js
+++ sec06-dynamic-content/nodejs/app.js
@@ -60,7 +60,7 @@ app.use("/favicon.ico", (req, res, next) => {
 app.use((req, res, next) => {
   // res.status(404).send("<h1>Page not found</h1>");
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
 });
 
 app.listen(3000);
