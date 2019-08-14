8006c4283940d5a8952a6dcefaad50ba5c748b91
Jack Gamboa <jgamboa929@gmail.com>
2019-08-14 11:42:28 -0500

    shop.ejs, add-product.ejs, 404.ejs - implement ejs.

--- sec06-dynamic-content/nodejs/app.js
+++ sec06-dynamic-content/nodejs/app.js
@@ -4,22 +4,31 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const expressHbs = require("express-handlebars");
-
 // app.set() is used to set a global configuration value on our express application.  We can use app.get() to get that value for the property.
-// We can provide 'reserved' keywords
-app.engine(
-  "hbs", // Give the engine any name (ie 'handlebars' is what we used), and call the function  below to initialze.
-  expressHbs({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs" // Applied to default layout out only.  Now this is configured to look for a default layout called main-layout.hbs and not main-layout.handlebars
-  }) // This is specific to configure 'layout' with handlebars
-);
-app.set("view engine", "hbs");
+// We can provide 'reserved' keywords to set like 'view engine', 'views', etc.
 
+// Set up for express-handlebars - unlike pug and ejs, we need to register handlebars engine.
+/*
+  const expressHbs = require("express-handlebars");
+  
+  app.engine(
+    "hbs", // Give the engine any name (ie 'handlebars' is what we used), and call the function  below to initialze.
+    expressHbs({
+      layoutsDir: "views/layouts",
+      defaultLayout: "main-layout",
+      extname: "hbs" // Applied to default layout out only.  Now this is configured to look for a default layout called main-layout.hbs and not main-layout.handlebars
+    }) // This is specific to configure 'layout' with handlebars
+  );
+  app.set("view engine", "hbs");
+  app.set("views", "views");
+*/
+
+// Set up for pug
 // app.set("view engine", "pug"); // pug is supported out of the box.  No need to require pug.
+// app.set("views", "views");
 
+// Set up for ejs.  Ejs is also supported out of the box.
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 // const adminRoutes = require("./routes/admin");
