d60ff8cef6a412a33c9ce2b97fea928372741a03
Jack Gamboa <jgamboa929@gmail.com>
2019-10-15 14:59:08 -0500

    Added edit-product page to display specific item to be updated.

--- sec09-routes-and-models/practice/nodejs-prac01-routes-and-models/nodejs/controllers/admin.js
+++ sec09-routes-and-models/practice/nodejs-prac01-routes-and-models/nodejs/controllers/admin.js
@@ -1,10 +1,21 @@
 const Product = require("../models/product");
 
+/* Replaced with Edit Product
 // Navigation link "Add Product"
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product" // Note property path value is arbitrary and will be used in our template to control UI features
+  });
+};
+*/
+
+// Navigation link is still "Add Product" but now renders "edit-product.ejs"
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product", // Note property path value is arbitrary and will be used in our template to control UI features
+    editing: false
   });
 };
 
@@ -14,6 +25,34 @@ exports.postAddProduct = (req, res, next) => {
 
   const product = new Product(title, imageUrl, description, price);
   product.save();
+  res.redirect("/");
+};
+
+// "Edit" button within "Admin Products" page - passed in .productId as params and .edit via query params
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit; // Extracted value is always a string like "true"
+  if (!editMode) {
+    res.redirect("/"); // For now just redirect
+  }
+
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product", // Here we do not want any navigation link highlighted.
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+// CONTINUE HERE
+exports.postEditProduct = (req, res, next) => {
+  console.log("check", req.body);
+
   res.redirect("/");
 };
 
--- sec09-routes-and-models/practice/nodejs-prac01-routes-and-models/nodejs/routes/admin.js
+++ sec09-routes-and-models/practice/nodejs-prac01-routes-and-models/nodejs/routes/admin.js
@@ -15,6 +15,12 @@ router.get("/products", adminController.getProducts);
 // POST /admin/add-product
 router.post("/add-product", adminController.postAddProduct);
 
+// GET /admin/edit-product/:productId
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+// POST /admin/edit-product - Will not need to receive any dynamic params because since it is a POST request we can get data from req.body.
+router.post("/edit-product", adminController.postEditProduct);
+
 // POST /admin/delete-product (will return to this)
 // router.post("/delete-product");
 
