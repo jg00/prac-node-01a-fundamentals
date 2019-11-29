a6af79cabc861cdcd5c3afbf759a8119bbd330fc
Jack Gamboa <jgamboa929@gmail.com>
2019-11-29 11:07:11 -0600

    Create order from cart items, display user orders.

--- sec12-mongodb/nodejs/controllers/shop.js
+++ sec12-mongodb/nodejs/controllers/shop.js
@@ -48,32 +48,6 @@ exports.getIndex = (req, res, next) => {
       });
     })
     .catch(err => console.log(err));
-
-  /* 'mysql2' package
-    Product.fetchAll()
-      .then(([rows, fieldData]) => {
-        // console.log(rows, fieldData);  // [[of rows], [of field attributes]]
-
-        res.render("shop/index", {
-          prods: rows,
-          pageTitle: "Shop",
-          path: "/"
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  */
-
-  /* File data source
-    Product.fetchAll(products => {
-      res.render("shop/index", {
-        prods: products,
-        pageTitle: "Shop",
-        path: "/"
-      });
-    });
-  */
 };
 
 // Navigation link "Cart"
@@ -89,34 +63,6 @@ exports.getCart = (req, res, next) => {
     })
     .catch(err => console.log);
 };
-
-/*
-  // Before User/Cart associated via Sequelize
-  // Navigation link "Cart"
-  exports.getCart = (req, res, next) => {
-    Cart.getCart(cart => {
-      // Remeber the cart only has the product id's, qty, and cart total.  However we also need produduct detail info.
-      Product.fetchAll(products => {
-        const cartProducts = [];
-        // Now that we have all the products we need to filter for products that are actually in the cart
-        for (product of products) {
-          const cartProductData = cart.products.find(
-            prod => prod.id === product.id
-          );
-          if (cartProductData) {
-            cartProducts.push({ productData: product, qty: cartProductData.qty }); // cartProducts.push(product) - You need the other info as well
-          }
-        }
-
-        res.render("shop/cart", {
-          pageTitle: "Your Cart",
-          path: "/cart",
-          products: cartProducts
-        });
-      });
-    });
-  };
-*/
 
 /*
   1 Fetch product
@@ -137,109 +83,15 @@ exports.postCart = (req, res, next) => {
     .catch(err => {
       console.log(err);
     });
-
-  // let fetchedCart;
-  // let newQuantity = 1; // initialized for products that do not exists yet
-
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     // Remember cart is only available in this anonymous function so that is why we create a fetchedCart variable outside to make cart available below.
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } }); // still returns an [] of products eventhough we only ask for a specfic product
-  //   })
-  //   .then(products => {
-  //     let product; // undefined if specific product was not found in the cart
-
-  //     // (1) If product already in cart, we need are only using it to get the product quantity in this .then() block
-  //     if (products.length > 0) {
-  //       product = products[0];
-
-  //       /* Side test only to see product instance methods and utilize them
-  //         console.log("PRODUCT", Object.keys(Product.prototype));
-  //         product.getCarts().then(carts => console.log(carts));
-  //         product.getUser().then(user => console.log(user));
-  //       */
-  //     }
-
-  //     // If there product exists (ie product is not undefined)
-  //     // -> Here we are only using the existing product to get the old quantity to set the new quantity.  We still return the existing product.
-  //     if (product) {
-  //       // At this point this is the product we found in our cart (ie exists in the cart)
-  //       // console.log("PROD EXISTS IN CART", product.cartItem); // Because of our n:m relation between Cart/Product we can access the 'joining table' we called cartItem
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       // return product; // Return existing product but at this point the newQuantity has been set accordinly
-  //       return Promise.resolve(product); // Same as return product
-  //     }
-
-  //     // (2) If we don't have a product yet, we know this product is not part of the cart yet.
-  //     // To add a product, we get the product object first and add that product to the cart.
-  //     // In addition we set some other fields in the 'join table'.
-  //     return Product.findByPk(prodId);
-  //   })
-
-  //   // We either get a new product to add or existing product to add.
-  //   // .addProduct() is how we would both 'add' (if does not exists) and 'edit' (if exists) a product and change the quantity accordingly.
-  //   // This is how you would also 'edit' quantity in the 'joining table'
-  //   // -> If it is a new product it INSERTS, if product exists, it updates
-  //   // INSERT INTO `cartItems` (`id`,`quantity`,`createdAt`,`updatedAt`,`cartId`,`productId`) VALUES (NULL,1,'2019-11-14 21:44:10','2019-11-14 21:44:10',1,4);
-  //   // UPDATE `cartItems` SET `quantity`=?,`updatedAt`=? WHERE `cartId` = ? AND `productId` = ?
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-
-  //   .then(() => {
-  //     res.redirect("/cart");
-  //   })
-
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
 };
 
-/* Before - Cart/Product association using Sequelize
-  // "Add to Cart" button in "Product Detail" page
-  exports.postCart = (req, res, next) => {
-    const prodId = req.body.productId;
-
-    Product.findById(prodId, product => {
-      Cart.addProduct(prodId, product.price);
-    });
-
-    res.redirect("/cart");
-  };
-*/
-
-/*
-      1 get user cart
-      2 get products in the cart
-      3 destroy the product in the cart
-  */
-// After User/Cart associated via Sequelize
 // "Delete" button from "Cart" page
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  // let fetchedCart; // Use if using .removeProduct approach below
 
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
+    .deleteItemFromCart(prodId)
 
-      // Another approach works as well
-      // return fetchedCart.removeProduct(product);
-
-      // console.log("HERE", product);
-      // console.log("PRODUCT QUANTITY", product.cartItem.quantity);
-    })
     .then(result => {
       // console.log("Item removed from the cart!");
       res.redirect("/cart");
@@ -249,120 +101,35 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-/* 
-  // Before User/Cart associated via Sequelize
-  // "Delete" button from "Cart" page
-  exports.postCartDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-      Cart.deleteProduct(prodId, product.price);
-      res.redirect("/cart");
-    });
-  };
- */
-
-/*
-  Post orders:
-  1 get user cart
-  2 get user cart items
-  3 create an order for the user
-  4 -> Add products to the order (for each product we need to 'correctly' add to the orderItem 'joining' table the quantity value)
-*/
+// Post order
 exports.postOrders = (req, res, next) => {
-  let fetchedCart;
-
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
+    .addOrder()
+    .then(result => {
+      res.redirect("/orders");
     })
-    .then(products => {
-      // console.log(products); // Product and the cartItem returned
-
-      // Create a user order an copy cart items to the order
-      req.user
-        .createOrder() // note creates an instance every time
-        .then(order => {
-          // console.log("ORDER STARTED", order);
-          // console.log("ORDER ITEMS", order.orderItem); // At this point we order.orderItem is "undefined" be we want to add rows to it with quantity values for each product.
-          /*
-            -> Here we are not adding a single product.  We are adding multiple products. So we have to set the 'quantity' correctly.
-            order.addProducts(products, {through: {quantity: {'set correctly for each product'}})
-            order.addProducts(products); // Side note, this will add entries into orderItems table but orderItems.quantity will be 'null'.
-          */
-
-          /* Check only - For each product we will set a property 'orderItem' that corresponds to our 'joining table' model
-            products.forEach(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              console.log(product);
-            });
-          */
-
-          /*
-            -> Each product needs to have a special field which is then understood
-            by Sequelize. To assign that special field we add product.orderItem
-            which Sequelize will look for due to our order/product Sequelize association we
-            created.
-            -> Set product.orderItem "stores JavaScript object" where you configure
-            the value for this 'joining table'.  
-          */
-          const updatedProducts = products.map(product => {
-            product.orderItem = { quantity: product.cartItem.quantity }; // Configure the value for the 'joining table' that Sequelize will look for.
-            return product;
-          });
-
-          return order.addProducts(updatedProducts);
-        })
-        .then(result => {
-          // After adding products to the order is successful we want to clear the cart
-          return fetchedCart.setProducts(null); // Removes all products related to the cart in the cartItems table.
-        })
-        .then(result => {
-          res.redirect("/orders");
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 /*
+  imageUrl:
+  'https://cdn.pixabay.com/photo/2016/03/31/20/51/book-1296045_960_720.png',
 
-product {
-    dataValues:
-     { id: 1,
-       title: 'Test Product 1',
-       price: 1.99,
-       imageUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/20/51/book-1296045_960_720.png',
-       description: 'prod desc 1',
-       createdAt: 2019-11-16T02:25:22.000Z,
-       updatedAt: 2019-11-16T02:25:22.000Z,
-       userId: 1,
-       cartItem: [cartItem] }  <- cartItem 
 */
 
 // Navigation link "Orders"
-// -> LEFT OUTER JOIN. 'Eager loading' example.
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] }) // -> With Order.belongsToMany(Product, {though: OrderItem}) we also 'include all' products [] array for each order.
+    .getOrders()
     .then(orders => {
-      /* Note this is returning [] of orders associated to user.  
-         In the view, orders.ejs, we loop though the orders array and for each order we 
-         have access to the 'joining table' using order.orderItem.
-      */
-      // console.log(orders); // products: [ [product], [product] ] } ] now added to each order with .getOrders({include: ["products"]})
-
       res.render("shop/orders", {
         pageTitle: "Your Orders",
         path: "/orders",
         orders: orders
       });
     })
-
     .catch(err => console.log(err));
 };
 
--- sec12-mongodb/nodejs/models/user.js
+++ sec12-mongodb/nodejs/models/user.js
@@ -95,6 +95,63 @@ class User {
       });
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new ObjectId(this._id) },
+      { $set: { cart: { items: updatedCartItems } } } // { cart: { items: [ {}, {}, {} ] } }
+    );
+  }
+
+  /*
+    1a Get cart items 'snapshot'
+    1b Create an order
+      - Want 'snapshot' of the products in the order colletions.  This is so we have a 'snapshot' of the product 'at the time' order placed.
+      - Want 'some' user data not all
+    2 Copy cart to the order
+    3 Empty cart items
+    4 Also clear user cart in the database
+  */
+  addOrder() {
+    const db = getDb();
+
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products, // Here we get all product info "at the time".  If product price changes, we still need the snapshot of what it was previvously. The price change should not affect the 'past' order so we do not want to update the price on the order.
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name // It's okay to keep some user info like name.  If this name changed we don't have to change it in all the orders.
+            // email: this.email // Again here we don't care about this info because user info could change.
+          }
+        };
+
+        return db.collection("orders").insertOne(order);
+      })
+      .then(result => {
+        this.cart = { items: [] };
+
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray(); // We know we return more than one so we can use toArray() instead of next()
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
