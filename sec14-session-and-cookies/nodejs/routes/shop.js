4c695d5289489b8fbc2e5e0165297c86d5a2cf63
Jack Gamboa <jgamboa929@gmail.com>
2019-12-04 11:48:09 -0600

    Start session and cookies

---
+++ sec14-session-and-cookies/nodejs/routes/shop.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const express = require("express");
+
+// const productsController = require("../controllers/products");
+const shopController = require("../controllers/shop");
+
+// const mainControllers = require("../controllers/mainController");
+
+const router = express.Router();
+
+// router.get("/", mainControllers.shopController);
+
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct);
+
+router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart);
+
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+
+router.post("/create-order", shopController.postOrders);
+
+router.get("/orders", shopController.getOrders);
+
+// router.get("/checkout", shopController.getCheckout);
+
+module.exports = router;
