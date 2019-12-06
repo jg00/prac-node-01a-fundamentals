2bdb2703222cf343a1424f95ce00f3eb5229f4a7
Jack Gamboa <jgamboa929@gmail.com>
2019-12-06 10:09:10 -0600

    Set up session.

--- sec14-session-and-cookies/nodejs/controllers/auth.js
+++ sec14-session-and-cookies/nodejs/controllers/auth.js
@@ -1,20 +1,20 @@
 // Navigation link "Login"
 exports.getLogin = (req, res, next) => {
   // Extract cookie -> isLoggedIn=true
-  const isLoggedIn =
-    req
-      .get("Cookie")
-      .split(";")[0]
-      .trim()
-      .split("=")[1] === "true";
+  // const isLoggedIn =
+  //   req
+  //     .get("Cookie")
+  //     .split(";")[0]
+  //     .trim()
+  //     .split("=")[1] === "true";
 
   // console.log(req.get("Accept-Language"));
 
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
-    isAuthenticated: isLoggedIn
-    // isAuthenticated: req.isLoggedIn
+    // isAuthenticated: isLoggedIn
+    isAuthenticated: req.isLoggedIn
   });
 };
 
@@ -25,6 +25,10 @@ exports.postLogin = (req, res, next) => {
   // req.isLoggedIn = true;  // Instead of seeting property on the request, we can send a cookie (via response header) back along with the response.
   // res.cookie("isLoggedInCookie", true); // Note this looks like it works as well.
 
-  res.setHeader("Set-Cookie", "loggedIn=true"); // Name of the header is 'Set-Cookie' and that is a reserved name. Second parameter is a key-value pair.
+  // res.setHeader("Set-Cookie", "loggedIn=true"); // Name of the header is 'Set-Cookie' and that is a reserved name. Second parameter is a key-value pair.
+  // res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10");
+  // res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10; Secure");
+  // res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=10"; Domain="");
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   res.redirect("/"); // Important this ends the request cycle and starts a new request.  Therefore the req.isLoggedIn property is no longer available on our new request.
 };
