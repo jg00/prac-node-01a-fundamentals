f52dd368cfb927998bbc52687fb6ce5b917c299e
Jack Gamboa <jgamboa929@gmail.com>
2019-12-11 16:28:06 -0600

    Add login and validate hashed password.

--- sec15-authorization/nodejs/controllers/auth.js
+++ sec15-authorization/nodejs/controllers/auth.js
@@ -25,42 +25,83 @@ exports.getSignup = (req, res, next) => {
 
 // Login button on Login page
 exports.postLogin = (req, res, next) => {
-  // const { email, password } = req.body;
-
-  /* 
-    2 Session - Instead of using a cookie.
-      - res.session -> session {} object
-      - here we can add any keys we want.
+  const { email, password } = req.body;
+  /*
+    1 Check email exists
+    2 Check hashed passwords match
+    3 Create/store a session to the database (done via MongoDBStore) and save session id on the clinet
   */
 
-  User.findById("5de6ad7bafb5db05b6cf3739")
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
+      if (!user) {
+        // return to not execute any code after this if block
+        return res.redirect("/login"); // Note a return will always resolve a promise and trigger a .then().  However we intentionally did not add a .then to not fire but just a .catch in case of errors.
+      }
 
-      /*
-        Note 
-        1a This is just the user data stored in our session object.(req.session.user.addToCart is not a function)
-        1b It does not include our user model functions.
-        2 (Important) For every new request, the session middleware does not go ahead
-        and fetch the user with the help of Mongoose.  It fetches the session data
-        from MongoDb and that is correct "but" for that, "it uses the MongoDBStore".
-        -> The MongoDBStore does not know about our Mongoose models.  So when it fetches the data
-        from MongoDB, it only fetches the data.  It does not fetch an object with all the methods
-        provided by Mongoose.
-      */
-      req.session.user = user; // What is stored in the session is just a regular user.
+      bcrypt
+        .compare(password, user.password) // In both cases matches or not, we still make it to the .then(result-boolean) block. What bcrypt does is takes the string password and compares if it's hashed version would match the hashed password we have in the database
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
 
-      // To be sure your session is saved to database before redirect use the req.session.save() to safely redirect.
-      req.session.save(() => {
-        res.redirect("/");
-      });
-
-      // res.redirect("/"); // Important this ends the request cycle and starts a new request.  Therefore the req.isLoggedIn property is no longer available on our new request.
+            // return to not execution of line res.redirect('/login') after this if block.
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch(err => {
+          // Error if bcrypt fails the compare(password, user.password)
+          console.log(err);
+          res.redirect("/login");
+        });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 };
+
+// Reference only - Login page wtih test user only
+// // Login button on Login page
+// exports.postLogin = (req, res, next) => {
+//   // const { email, password } = req.body;
+
+//   /*
+//     2 Session - Instead of using a cookie.
+//       - res.session -> session {} object
+//       - here we can add any keys we want.
+//   */
+
+//   User.findById("5de6ad7bafb5db05b6cf3739")
+//     .then(user => {
+//       req.session.isLoggedIn = true;
+
+//       /*
+//         Note
+//         1a This is just the user data stored in our session object.(req.session.user.addToCart is not a function)
+//         1b It does not include our user model functions.
+//         2 (Important) For every new request, the session middleware does not go ahead
+//         and fetch the user with the help of Mongoose.  It fetches the session data
+//         from MongoDb and that is correct "but" for that, "it uses the MongoDBStore".
+//         -> The MongoDBStore does not know about our Mongoose models.  So when it fetches the data
+//         from MongoDB, it only fetches the data.  It does not fetch an object with all the methods
+//         provided by Mongoose.
+//       */
+//       req.session.user = user; // What is stored in the session is just a regular user.
+
+//       // To be sure your session is saved to database before redirect use the req.session.save() to safely redirect.
+//       req.session.save(() => {
+//         res.redirect("/");
+//       });
+
+//       // res.redirect("/"); // Important this ends the request cycle and starts a new request.  Therefore the req.isLoggedIn property is no longer available on our new request.
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// };
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
@@ -71,9 +112,11 @@ exports.postSignup = (req, res, next) => {
         return res.redirect("/signup"); // Remember this return will return a promise and 'will' execute the next .then block.  Note we didnt need a .then since we recirected.
       }
 
+      // Only executed if we make it to the hashing step below (ie no user document found)
       return bcrypt
         .hash(password, 12) // returns a promise
         .then(hashedPassword => {
+          // Remember that in our User Mongoose Schema user and email are required (May be good place for validation)
           const user = new User({
             email: email,
             password: hashedPassword,
@@ -91,7 +134,7 @@ exports.postSignup = (req, res, next) => {
     });
 };
 
-// Chain above
+// Nested after the .hash() above
 // .then(hashedPassword => {
 //   const user = new User({
 //     email: email,
@@ -114,7 +157,7 @@ exports.postLogout = (req, res, next) => {
   // console.log("test logout");
   // All session data will now be lost.
   req.session.destroy(err => {
-    console.log(err);
+    // console.log(err);
     res.redirect("/login");
   });
 };
