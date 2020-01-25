f9bff257af546b762227e2a986b7720ab737fba2
Jack Gamboa <jgamboa929@gmail.com>
2020-01-24 23:47:30 -0600

    Backup simple REST Api project.

---
+++ sec24-rest-api-basics/backup/nodejs-rest-api-copy-v01-complete-simple-restapi-project/controllers/feed.js
@@ -0,0 +1,19 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is the first posts!" }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  console.log(title, content);
+
+  // Create post in db
+
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+};
