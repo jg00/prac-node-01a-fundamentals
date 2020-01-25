f7fa82e93841fdb4c5ff3a6531bcd2e913e9c7f5
Jack Gamboa <jgamboa929@gmail.com>
2020-01-24 23:42:44 -0600

    Simple REST Api, set up CORS fix.

--- sec24-rest-api-basics/nodejs-rest-api/app.js
+++ sec24-rest-api-basics/nodejs-rest-api/app.js
@@ -4,7 +4,23 @@ const app = express();
 const feedRoutes = require("./routes/feed");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // application/json
+
+// Deal with CORS issue
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"), // '*' allow all domains that should be alble to access our server or specific client like 'codepen.io'.
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE"
+    ), // We tell the clients (ie the Origins) that they are only allowed to use specific http methods.
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    ); // '*' or specific headers our clients are allowed to set on their requests.
+
+  next();
+});
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, () => console.log("test"));
+app.listen(8080, () => console.log("Server started"));
